4a92c2afca9f31258da76b8d060ddb3ed1170d0b
Sean.Song <Sean.Song@infotools.com>
2024-11-27 16:53:42 +1300

    add js file and method

---
+++ wwwroot/js/PropertyList.js
@@ -0,0 +1,13 @@
+﻿export function showModal(modalId) {
+    const modal = new bootstrap.Modal(document.getElementById(modalId), {
+        backdrop: 'static',
+        keyboard: false,
+    });
+    modal.show();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btnFilter").addEventListener("click", function () {
+        showModal('filterModal');
+    });
+});
